13909fa4925f14e36771aa08bb07988cd4c29d78
Rawan <120096605+rwnmtwly@users.noreply.github.com>
2024-07-06 23:01:31 +0300

    starting over

--- MVC/core/exception/ForbiddenException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace app\core\exception;
-class ForbiddenException extends \Exception{
-    protected $message = 'you don\'t have permission to access this page';
-    protected $code = '403';
-}
-?>
