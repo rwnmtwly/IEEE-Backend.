4dd47ba49f10cb9f4e5e068cbae91946bb8f4f13
Rawan <120096605+rwnmtwly@users.noreply.github.com>
2024-07-17 20:11:05 +0300

    Task no. 22: new code

---
+++ Laravel/example/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\JobController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::view('/','home');
+Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class);
+
